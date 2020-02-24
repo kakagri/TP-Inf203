c8018800729ac7e0b3a429799b68ac31956ab5e6
Khaled Grira <kh.grira@gmail.com>
2020-02-24 16:32:57 +0100

    exercise3 done

--- JavaScript/exercise2.js
+++ JavaScript/exercise2.js
@@ -1,9 +1,13 @@
+/* tp INF203 - Document rédigé par Khaled GRIRA */
+
+
+// Question 2A
 function wordCount(phrase){
   var vect= phrase.split(" ");
   var result={};
   for(var k=0;k<vect.length; k++){
     var mot = vect[k];
-    if(result[mot]==NaN || result[mot]==undefined){
+    if(result[mot]===NaN || result[mot]===undefined){
       result[mot]=1;
     }
     else{
@@ -14,4 +18,88 @@ function wordCount(phrase){
 }
 
 
-console.log(wordCount("bonjour je suis khaled grira grira grira khaled khaled "));
+// Question 2B
+function WordList(phrase){
+  this.listOfCount= wordCount(phrase);
+  var that= this;
+
+  this.maxCountWord= function (){
+    var maximum=0;
+    var currentMaxWord;
+
+    for(var word in that.listOfCount){
+
+      var occOfWord=that.listOfCount[word];
+      if(occOfWord>maximum){
+        maximum=occOfWord;
+        currentMaxWord=word;
+      }
+      if(occOfWord===maximum){
+        if(word<=currentMaxWord){
+          currentMaxWord=word;
+        }
+      }
+    }
+      return currentMaxWord
+  }
+
+
+  this.minCountWord= function (){
+    var minimum;
+    var currentMinWord;
+
+    for(var word in that.listOfCount){
+
+      if(minimum===undefined){
+        minimum=that.listOfCount[word];
+        currentMinWord=word;
+        listOfMinWords=[currentMinWord];
+      }
+      else{
+
+      var occOfWord=that.listOfCount[word];
+      if(occOfWord<minimum){
+        minimum=occOfWord;
+        currentMinWord=word;
+      }
+      if(occOfWord===minimum){
+        if(word<=currentMinWord){
+          currentMinWord=word;
+        }
+      }
+    }
+
+    }
+      return currentMinWord
+  }
+
+  this.getWords=function(){
+    var vecteur=[];
+    for(var mot in that.listOfCount){
+      vecteur.push(mot);
+    }
+    vecteur.sort();
+    return vecteur;
+  }
+
+  this.getCount=function (word){
+    var nmb=that.listOfCount[word];
+    if(nmb===NaN || nmb===undefined){
+      return 0;
+    }
+    else{
+      return nmb;
+    }
+  }
+// Question 2C
+
+  this.applyWordFunc=function(f){
+    var vectOfWords=that.getWords();
+    return vectOfWords.map(f);
+  }
+
+}
+
+
+exports.wordCount = wordCount;
+exports.WordList = WordList;
---
+++ JavaScript/exercise3.js
@@ -0,0 +1,34 @@
+/* tp INF203 - Document rédigé par Khaled GRIRA */
+
+
+// Question 3A
+
+function Student(lastName,firstName,id){
+  this.lastName=lastName;
+  this.firstName=firstName;
+  this.id=id;
+  var that=this;
+// Question 3B
+  this.toString = function(){
+    var result= "student: "+[that.lastName,that.firstName,that.id].join(", ");
+    return result;
+  }
+
+}
+
+//Question 3C
+function ForeignStudent(lastName,firstName,id,nationality){
+  Student.call(this,lastName,firstName,id);
+  this.nationality=nationality;
+  var that=this;
+
+  this.toString = function(){
+    var result= "student: "+[that.lastName,that.firstName,that.id,that.nationality].join(", ");
+    return result;
+  }
+
+}
+
+console.log(stud.toString())
+exports.Student = Student;
+exports.ForeignStudent = ForeignStudent;
