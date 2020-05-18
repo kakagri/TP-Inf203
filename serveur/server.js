4768705ce5bd494d1dbab22ec3ca2fe0205ec3bc
kakagri <kakagri@kakagri.lan>
2020-05-18 20:02:50 +0200

    hello2

--- serveur/server.js
+++ serveur/server.js
@@ -1,7 +1,8 @@
 var http=require('http'),
 path=require('path'),
 url=require('url'),
-fs=require('fs'),mime=require('mime-types'),querystring=require('querystring');
+fs=require('fs'),mime=require('mime-types'),querystring=require('querystring'),
+striptags=require('striptags');
 var port=parseInt(process.argv[2]);
 var server= http.createServer(function(request,response){
   var my_path=url.parse(request.url).pathname;
@@ -29,17 +30,48 @@ var server= http.createServer(function(request,response){
 
       }
       else{
+        if(my_path==='/hello2'){
+          var name=my_url.query['name'].trim();
+          name=striptags(name);
+          fs.readFile('./names.txt','utf-8',
+            function(error,data){
+              if(error){
+                response.writeHead(404);
+                response.write('Erreur: fichier non trouve');}
+              else{
+                  response.writeHead(200,"OK",{'Content-type':'text/html'});
+                  var rep="<!DOCTYPE html><head><meta charset='utf-8'></head><body>Hello "+name+", the following users have already visited this page:"+data+'</body></html>'
+                  response.write(rep);
+            }
+            response.end();
+          });
+
+          fs.appendFile('./names.txt', " "+name+",", function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+          });
+        }
+        else{
+          if(my_path=='/clear'){
+            fs.writeFile('names.txt', '', function (err) {
+              if (err) return console.log(err);
+              console.log('enregistre');
+              response.writeHead(200);
+              response.end();
+            });}
+          else{
           fs.readFile('.'+my_path,
             function(error,data){
               if(error){
                 response.writeHead(404);
                 response.write('Erreur: fichier non trouve');}
                 else{
-                  response.writeHead({'Content-type':mime.lookup(my_path)});
+                  response.writeHead(200,"OK",{'Content-type':mime.lookup(my_path)});
                   response.write(data);
             }
             response.end();
-          });
+          });}
+        }
 
         }
     }
